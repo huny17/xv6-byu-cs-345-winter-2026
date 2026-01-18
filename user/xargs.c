357276ece3f19e0ceff4034531a5a5f0703df3e8
Adelyn Meeks <honey17@byu.edu>
2026-01-17 18:31:04 -0700

    tried to do part 4 of lab

--- user/xargs.c
+++ user/xargs.c
@@ -1,1 +1,40 @@
-//part 4
+// #include "kernel/types.h"
+// #include "user/user.h"
+// #include "kernel/param.h"
+
+// int
+// main(int argc, char *argv[])
+// {
+//     int pid, status;
+//     //char input[argc][MAXARG];
+//     char buf[100];
+    
+//     pid = fork();
+
+//     if(pid == 0){   //child
+//         int i = 0;
+//         for(i; i < argc; i++){
+//             int length = read(0, buf, 1);
+//             //input[i] = buf;
+//             if(strcmp(argv[i], "\n") == 0 || length == 0){
+//                 break;
+//             }
+//         }
+//         exec(buf);
+//     } 
+    
+//     else {        //parent
+//         wait(&status);
+//         int i = 0;
+//         for(i; i <= argc; i++){
+//             int length = read(0, buf, 1);
+//             if(strcmp(argv[i], "\n") == 0 || length == 0){
+//                 break;
+//             }
+//         }
+        
+//         exec(buf);
+//     }
+
+//     exit(0);
+// }
