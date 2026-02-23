358e68b1dca7ddfeb0e25df93f8b0601991b927b
Adelyn Meeks <honey17@byu.edu>
2026-02-23 00:59:30 -0700

    might go back to get late credit

--- user/xargs.c
+++ user/xargs.c
@@ -1,40 +1,40 @@
-// #include "kernel/types.h"
-// #include "user/user.h"
-// #include "kernel/param.h"
+#include "kernel/types.h"
+#include "user/user.h"
+#include "kernel/param.h"
 
-// int
-// main(int argc, char *argv[])
-// {
-//     int pid, status;
-//     //char input[argc][MAXARG];
-//     char buf[100];
+int
+main(int argc, char *argv[])
+{
+    int pid, status;
+    //char input[argc][MAXARG];
+    char buf[100];
     
-//     pid = fork();
+    pid = fork();
 
-//     if(pid == 0){   //child
-//         int i = 0;
-//         for(i; i < argc; i++){
-//             int length = read(0, buf, 1);
-//             //input[i] = buf;
-//             if(strcmp(argv[i], "\n") == 0 || length == 0){
-//                 break;
-//             }
-//         }
-//         exec(buf);
-//     } 
+    if(pid == 0){   //child
+        int i = 0;
+        for(i; i < argc; i++){
+            int length = read(0, buf, 1);
+            //input[i] = buf;
+            if(strcmp(argv[i], "\n") == 0 || length == 0){
+                break;
+            }
+        }
+        exec(buf);
+    } 
     
-//     else {        //parent
-//         wait(&status);
-//         int i = 0;
-//         for(i; i <= argc; i++){
-//             int length = read(0, buf, 1);
-//             if(strcmp(argv[i], "\n") == 0 || length == 0){
-//                 break;
-//             }
-//         }
+    else {        //parent
+        wait(&status);
+        int i = 0;
+        for(i; i <= argc; i++){
+            int length = read(0, buf, 1);
+            if(strcmp(argv[i], "\n") == 0 || length == 0){
+                break;
+            }
+        }
         
-//         exec(buf);
-//     }
+        exec(buf);
+    }
 
-//     exit(0);
-// }
+    exit(0);
+}
